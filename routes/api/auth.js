db17a60553840f440801f77636f2569139f7606f
Filatov44 <filatov300@gmail.com>
2023-01-05 14:00:00 +0200

    add register

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,17 @@
 const express = require("express");
 
+const ctrlAuth = require("../../controllers/auth");
+const {
+//   validateParams,
+  validateBody,
+//   validateParamsForMangoose,
+} = require("../../middlewares");
+
+const schemas = require("../../schemas/users");
+
 const router = express.Router();
+
+// signup
+router.post("/signup", validateBody(schemas.joiRegisterSchema), ctrlAuth.register);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ require("dotenv").config();
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
+mongoose.set("strictQuery", true);
+
 mongoose
   .connect(DB_HOST)
   .then(() =>
