9fc13852e3774875a66b4c5b2f77d5273a8cbfe0
Filatov44 <filatov300@gmail.com>
2023-01-10 15:28:58 +0200

    add jimp

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,30 +1,37 @@
 const User = require("../../models/user");
 const fs = require("fs/promises");
 const path = require("path");
+const Jimp = require("jimp");
 
 // создаем путь к папке аватар
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   // забираем значение пути в tempUpload и originalname
-    const { path: tempUpload, originalname } = req.file;
-    const { _id } = req.user;
-    // делаем уникальное имя файла
-    const fileName = `${_id}_${originalname}`;
+  const { path: tempUpload, originalname } = req.file;
+  const { _id } = req.user;
+  // делаем уникальное имя файла
+  const fileName = `${_id}_${originalname}`;
 
   const resultUpload = path.join(avatarsDir, fileName);
-  // перемещаем файд в нужную папку
-    await fs.rename(tempUpload, resultUpload);
-    
-    const avatarURL = path.join("avatars", fileName);
 
-    // сохраняем в базе
-    await User.findByIdAndUpdate(_id, { avatarURL });
-
-    res.json({
-        avatarURL,
+  Jimp.read(tempUpload)
+    .then((image) => {
+      return image.resize(250, 250).write(resultUpload);
     })
-    
-}
+      .catch((error) =>  console.error(error) );
+
+  // перемещаем файл в нужную папку
+  await fs.rename(tempUpload, resultUpload);
+
+  const avatarURL = path.join("avatars", fileName);
+
+  // сохраняем в базе
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
 
 module.exports = updateAvatar;
